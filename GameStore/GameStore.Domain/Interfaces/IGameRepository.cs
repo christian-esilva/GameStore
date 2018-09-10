6b682aca82667208e834112e7b29c7c42ea3643b
Christian Eufrasio da Silva <christian.eds@hotmail.com>
2018-09-10 09:13:17 -0300

    IGameRepository

---
+++ GameStore/GameStore.Domain/Interfaces/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using GameStore.Domain.Entities;
+using System.Collections.Generic;
+
+namespace GameStore.Domain.Interfaces
+{
+    public interface IGameRepository : IRepository<Game>
+    {
+        List<Game> GetWithCategory(int skip = 0, int take = 30);
+        Game GetWithCategory(int id);
+    }
+}
